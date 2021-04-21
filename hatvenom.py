f1fae852b10bd675b406ca018390020d73e061f8
Ivan Nikolsky <enty8080@gmail.com>
2021-04-21 20:46:54 +0300

    Update hatvenom.py

--- hatvenom.py
+++ hatvenom.py
@@ -5,7 +5,7 @@ import argparse
 from core.payload import PayloadGenerator
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--format', dest='platform', help='Platform to generate for.')
+parser.add_argument('--format', dest='format', help='Platform to generate for.')
 parser.add_argument('--architeture', dest='architecture', help='Architecture to generate for.')
 parser.add_argument('--shellcode', dest='shellcode', help='Shellcode to inject.')
 parser.add_argument('-o', '--output', dest='output', help='File to output generated payload.')
