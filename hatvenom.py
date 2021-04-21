6593ca391260fcf03527b5bd3248bc1128c7aaa5
Ivan Nikolsky <enty8080@gmail.com>
2021-04-21 20:44:08 +0300

    Create hatvenom.py

---
+++ hatvenom.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from core.payload import PayloadGenerator
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--format', dest='platform', help='Platform to generate for.')
+parser.add_argument('-a', '--architeture', dest='architecture', help='Architecture to generate for.')
+parser.add_argument('-s', '--shellcode', dest='shellcode', help='Shellcode to inject.')
+parser.add_argument('-o', '--output', dest='output', help='File to output generated payload.')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    if args.format and args.architecture:
+        pg = PayloadGenerator()
+        shellcode = bytes(args.shellcode) if args.shellcode else b'\x00'
+        filename = args.output if args.output else 'a.out'
+
+        print("[*] Generating payload...")
+        payload = pg.generate(args.format, args.architecture, bytes(args.shellcode))
+        print(f"[i] Final payload size: {str(len(payload))}")
+
+        print(f"[*] Saving payload to {filename}...")
+        with open(filename, 'wb') as f:
+            f.write(payload)
+        print(f"[+] Payload saved to {filename}!")
+        sys.exit(1)
+    else:
+        print("[-] No format and architecture specified!")
+
+    print("[-] Failed to generate payload!")
+    sys.exit(1)
