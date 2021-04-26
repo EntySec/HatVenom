37aa97fc32dcd73c99255edb51ccfac877665170
Ivan Nikolsky <enty8080@gmail.com>
2021-04-26 22:48:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(name='hatvenom',
       author_email='entysec@gmail.com',
       license='MIT',
       packages=['hatvenom'],
-      zip_safe=False,
-      scripts=('hatvenom',),
-      entry_points={}
+      zip_safe=False
 )
 
 os.system("mkdir ~/.hatvenom")
 os.system("cp -r templates ~/.hatvenom")
+os.system("cp hatvenom.py /usr/local/bin/hatvenom")
+os.system("chmod +x /usr/local/bin/hatvenom")
