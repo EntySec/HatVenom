48326b946dd6d6190bff35854bd12e546b57bb55
Ivan Nikolsky <enty8080@gmail.com>
2021-05-19 22:31:25 +0300

    Update cli.py

--- hatvenom/cli.py
+++ hatvenom/cli.py
@@ -81,3 +81,10 @@ class HatVenomCLI(PayloadGenerator):
 
         print("Failed to generate payload.")
         sys.exit(1)
+
+@click.command()
+def main():
+    cli = HatVenomCLI()
+    cli.start()
+
+main()
