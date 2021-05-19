e1ed4d6797b5d6add174971c3194314248ad0e3c
Ivan Nikolsky <enty8080@gmail.com>
2021-05-19 22:22:46 +0300

    Create hatvenom.py

---
+++ hatvenom/hatvenom.py
@@ -0,0 +1,16 @@
+class HatVenom(PayloadGenerator):
+    def ip_bytes(self, ip):
+        return self.ip_to_bytes(ip)
+
+    def port_bytes(self, port):
+        return self.port_to_bytes(port)
+
+    def string_bytes(self, string):
+        return self.string_to_bytes(string)
+
+    def generate(self, file_format, arch, shellcode, offsets={}):
+        return self.generate_payload(file_format, arch, shellcode, offsets)
+
+    def generate_to(self, file_format, arch, shellcode, offsets={}, filename='a.out'):
+        with open(filename, 'wb') as f:
+            f.write(self.generate_payload(file_format, arch, shellcode, offsets))
