1ee66cda084c48f63db2f2064af28efd151a6290
Ivan Nikolsky <enty8080@gmail.com>
2021-07-25 21:36:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,10 +41,5 @@ setup(name='hatvenom',
                 "hatvenom = hatvenom.cli:main"
           ]
       },
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.8",
-      ],
       zip_safe=False
 )
