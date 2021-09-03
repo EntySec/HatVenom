d8afa7af013c21f630538002ac846ceb43862172
Ivan Nikolsky <enty8080@gmail.com>
2021-09-03 23:40:11 +0300

    Update generator.py

--- hatvenom/generator.py
+++ hatvenom/generator.py
@@ -39,6 +39,36 @@ class PayloadGenerator:
         'aarch64': templates + 'macho_aarch64.bin'
     }
 
+    def generate_macho(self, arch, data):
+        if arch in self.macho_templates.keys():
+            if os.path.exists(self.macho_templates[arch]):
+                if len(data) >= len('PAYLOAD:'):
+                    macho_file = open(self.macho_templates[arch], 'rb')
+                    macho = macho_file.read()
+                    macho_file.close()
+
+                    payload_index = macho.index(b'PAYLOAD:')
+                    content = macho[:payload_index] + data + macho[payload_index + len(data):]
+                    return content
+        return None
+    
+    def generate_payload(self, file_format, arch, data, offsets={}):
+        if file_format in self.formats.keys():
+            for offset in offsets.keys():
+                if (':' + offset + ':ip:').encode() in data:
+                    data = data.replace((':' + offset + ':ip:').encode(), self.ip_to_bytes(offsets[offset]))
+                elif (':' + offset + ':port:').encode() in data:
+                    data = data.replace((':' + offset + ':port:').encode(), self.port_to_bytes(offsets[offset]))
+                elif (':' + offset + ':string:').encode() in data:
+                    data = data.replace((':' + offset + ':string:').encode(), self.string_to_bytes(offsets[offset]))
+                elif (':' + offset + ':').encode() in data:
+                    sub = offsets[offset] if isinstance(offsets[offset], bytes) else codecs.escape_decode(offsets[offset], 'hex')[0]
+                    data = data.replace((':' + offset + ':').encode(), sub)
+                else:
+                    return None
+            return self.formats[file_format](self, arch, data)
+        return None
+    
     @staticmethod
     def ip_to_bytes(host):
         result = b""
@@ -56,45 +86,3 @@ class PayloadGenerator:
         string = string.encode().hex()
         string = '\\x' + '\\x'.join(a + b for a, b in zip(string[::2], string[1::2]))
         return codecs.escape_decode(string, 'hex')[0]
-
-    def generate_payload(self, file_format, arch, data, offsets={}):
-        if file_format in self.formats.keys():
-            for offset in offsets.keys():
-                if (':' + offset + ':ip:').encode() in data:
-                    data = data.replace((':' + offset + ':ip:').encode(), self.ip_to_bytes(offsets[offset]))
-                elif (':' + offset + ':port:').encode() in data:
-                    data = data.replace((':' + offset + ':port:').encode(), self.port_to_bytes(offsets[offset]))
-                elif (':' + offset + ':string:').encode() in data:
-                    data = data.replace((':' + offset + ':string:').encode(), self.string_to_bytes(offsets[offset]))
-                elif (':' + offset + ':').encode() in data:
-                    sub = offsets[offset] if isinstance(offsets[offset], bytes) else codecs.escape_decode(offsets[offset], 'hex')[0]
-                    data = data.replace((':' + offset + ':').encode(), sub)
-                else:
-                    return None
-            return self.formats[file_format](self, arch, data)
-        return None
-
-    def generate_macho(self, arch, data):
-        if arch in self.macho_templates.keys():
-            if os.path.exists(self.macho_templates[arch]):
-                if len(data) >= len('PAYLOAD:'):
-                    macho_file = open(self.macho_templates[arch], 'rb')
-                    macho = macho_file.read()
-                    macho_file.close()
-
-                    payload_index = macho.index(b'PAYLOAD:')
-                    content = macho[:payload_index] + data + macho[payload_index + len(data):]
-                    return content
-        return None
-
-    def generate_raw(self, arch, data):
-        if arch in ['generic']:
-            return data
-        return None
-
-    formats = {
-        'pe': generate_pe,
-        'elf': generate_elf,
-        'macho': generate_macho,
-        'raw': generate_raw
-    }
