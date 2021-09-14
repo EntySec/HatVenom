08ab7c3067977a3933fdd0876410ff31cd024ce9
Ivan Nikolsky <enty8080@gmail.com>
2021-09-14 16:52:40 +0300

    Update cli.py

--- hatvenom/cli.py
+++ hatvenom/cli.py
@@ -42,7 +42,7 @@ class StoreDictKeyPair(argparse.Action):
 
 
 class HatVenomCLI(Generator, Badges):
-    description = "Powerful payload generation and shellcode injection tool that provides support for common platforms and architectures."
+    description = "HatVenom is a HatSploit native powerful payload generation and shellcode injection tool that provides support for common platforms and architectures."
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-f', '--format', dest='format', help='Platform to generate for.')
     parser.add_argument('-a', '--arch', dest='arch', help='Architecture to generate for.')
