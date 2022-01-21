eba44527803304c2ebff44217006b61db5846a66
Ivan Nikolsky <enty8080@gmail.com>
2022-01-21 09:53:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 setup(name='hatvenom',
       version='2.0.0',
-      description='HatVenom is a HatSploit native powerful payload generation and shellcode injection tool that provides support for common platforms and architectures.',
+      description='HatVenom is a HatSploit native powerful payload generation tool that provides support for all common platforms and architectures.',
       url='http://github.com/EntySec/HatVenom',
       author='EntySec',
       author_email='entysec@gmail.com',
